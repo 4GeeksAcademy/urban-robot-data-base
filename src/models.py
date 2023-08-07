b9b3b7671cd6c4e3f4b631a33b0840f11eba42c1
Yanfe Pedroza Alberto <38539546+Yanfep@users.noreply.github.com>
2023-08-07 19:45:20 +0000

    python update

--- src/models.py
+++ src/models.py
@@ -7,23 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(255), unique=True, nullable=False)
+    firstname = Column(String(255))
+    lastname = Column(String(255))
+    email = Column(String(255), unique=True, nullable=False)
+
+class Favorites(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Comment(Model):
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Post(Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Media(Model):
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50))  # You might want to use an Enum here
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Planets(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255))
+
+class Characters(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255))
+    
 
     def to_dict(self):
         return {}
